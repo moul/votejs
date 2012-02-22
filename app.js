c9750633c117e57aefc375302a69cb8d46b92e55
Manfred Touron <github@42.am>
2012-02-22 15:25:27 +0100

    global redesign

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express'),
   app = express.createServer(),
   io = require('socket.io').listen(app),
-  db = {};
-
+  db = {
+      users: [
+          { id: 0, name: 'admin', email: 'm@42.am', role: 'admin' }
+      ]
+  };
 
 
 app.configure(function() {
@@ -10,26 +13,27 @@ app.configure(function() {
                   app.set('view engine', 'jade');
                   app.use(express.bodyParser());
 
-                  require('./boot')(app, db);
-
-                  app.use(app.router);
                   app.use(express.methodOverride());
                   app.use(express.logger({ format: ':method :url' }));
-                  app.use(express.logger({ format: ':method :url' }));
-                  app.use('/public', express.static(__dirname + '/public'));
-                  app.use('/', express.static(__dirname + '/static/pages/index.html'));
-                  app.use(express.cookieParser());
-                  app.use(express.session({ secret: "keyboard cat" }));
                   app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+
+                  app.use(express.cookieParser('keyboard cat'));
+                  app.use(express.session({ secret: "keyboard cat" }));
+
+                  app.use(app.router);
+                  app.use('/public', express.static(__dirname + '/public'));
 });
 
-app.listen(3000);
+require('./routes/site')(app, db);
+require('./routes/vote')(app, db);
 
-/*app.get('/', function(req, res) {
-            res.render('index.jade', { title: 'Test!', pretty: false });
-});
+if (!module.parent) {
+    var port = 3000;
+    app.listen(port);
+    console.log('Express started on port ' + port);
+}
 
-io.sockets.on('connection', function(socket) {
+/*io.sockets.on('connection', function(socket) {
                   console.log('new connection');
                   socket.emit('welcome', { test: 'test' });
 });*/
